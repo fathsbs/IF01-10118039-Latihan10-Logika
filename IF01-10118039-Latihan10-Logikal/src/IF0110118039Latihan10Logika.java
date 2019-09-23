161bb8cb115c6c87105bc79e17af6b022ca22de4
fathsbs <55565303+fathsbs@users.noreply.github.com>
2019-09-23 20:08:40 +0700

    Add files via upload

---
+++ IF01-10118039-Latihan10-Logikal/src/IF0110118039Latihan10Logika.java
@@ -0,0 +1,20 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM :   
+ */
+
+public class IF0110118039Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status dewasa : " + dewasa);
+    }
+
+}
